0a69c1390fbbb368ca456958dfd01a7295eacba9
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-06-07 23:57:40 +0100

    feat: add views, urls and templates to blog

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -8,7 +8,7 @@ class PublishedManager(models.Manager):
     Returns the QuerySet to be executed.
     """
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status=published)
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -29,6 +29,9 @@ class Post(models.Model):
     
     class Meta:
         ordering = ('-publish',)
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
     
     def __str__(self):
         return self.title
