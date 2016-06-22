69161e24582e55163fde152451eb0583aa11055a
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-06-22 18:57:56 +0100

    feat: add Comment model and register it in admin site

--- blog/mysite/blog/admin.py
+++ blog/mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     # set fields of the model to be displayed in the admin object list page
@@ -11,5 +11,12 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish' # adds a date hierachy, which can be used to navigate (at the top)
     ordering = ['status', 'publish']
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
 # register Post model into the admin site using a custom class, which inherits form admin.ModelAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
