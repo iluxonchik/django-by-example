59238da6858b6d6c91ef60264d7132ee00d6a1d2
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-06-23 16:24:00 +0100

    feat: implement similar posts

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -5,6 +5,7 @@ from .forms import EmailPostForm, CommentForm
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -57,6 +58,14 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
         context["comment_form"] = comment_form
+
+    # List of similiar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    # annotate() -> COUNT tags ... GROUP BY post.id
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4] 
+    context["similar_posts"] = similar_posts
+    
     return render(request, 'blog/post/detail.html', context)
 
 def post_share(request, post_id):
