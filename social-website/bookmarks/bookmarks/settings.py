c2c505c5b55dc89f9750d4aa0f11d9501641e9e1
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-06-27 01:59:07 +0100

    feat: implement password rest
    * EMAIL_BACKEND setting modified to output emails to console

--- social-website/bookmarks/bookmarks/settings.py
+++ social-website/bookmarks/bookmarks/settings.py
@@ -17,6 +17,8 @@ LOGIN_REDIRECT_URL = reverse_lazy('dashboard') # where to redirect after the use
 LOGIN_URL = reverse_lazy('login') # to which url the user should be redirected to login
 LOGOUT_URL = reverse_lazy('logout')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
