88c67196c76d5bec865a0aee39b5d939e3b3d12d
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-06-30 17:19:11 +0100

    feat: add Contact model
    User now has a many-to-many realtionship with itself through an intermediate model "Contact" (used for follow functionality)

--- social-website/bookmarks/account/models.py
+++ social-website/bookmarks/account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
@@ -9,3 +10,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
+    user_to = models.ForeignKey(User, related_name='rel_to_set')
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
+
+# Add the follwing field to the built-in User model dynamically
+# Creating a many-to-many relationship through an intermediate model ("Contact")
+User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
