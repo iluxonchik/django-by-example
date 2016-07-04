6ef542c1bb8540bf5f8189a8e146ef0cfeec0790
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-04 14:29:39 +0100

    feat: register Product and Category models in admin

--- online-shop/myshop/shop/admin.py
+++ online-shop/myshop/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name', )}  # NOTE: 'slug' field is of type 'models.SlugField'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']  # fields that are editable in the list display in admin dashboard
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
