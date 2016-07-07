2af0318368bb0b7afb3159b0d69c1e8fc350e243
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-07 15:04:53 +0100

    feat: translate urls and add ability to change language

--- online-shop/myshop/payments/urls.py
+++ online-shop/myshop/payments/urls.py
@@ -1,8 +1,9 @@
 from .import views
 from django.conf.urls import url
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
-    url(r'^done/$', views.payment_done , name='done'),
-    url(r'^cancelled/$', views.payment_cancel , name='cancel'),
-    url(r'^process/$', views.payment_process , name='process'),
+    url(_(r'^done/$'), views.payment_done , name='done'),
+    url(_(r'^cancelled/$'), views.payment_cancel , name='cancel'),
+    url(_(r'^process/$'), views.payment_process , name='process'),
 ]
