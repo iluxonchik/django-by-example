3c4354e63e55e60a695d46ab6a14f98a5f72ff48
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-14 15:15:37 +0100

    chore: change project name and file directories

--- elearning_platform/educa/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EducaConfig(AppConfig):
-    name = 'educa'
--- elearning_platform/educa/educa/wsgi.py
+++ elearning_platform/educa/educa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for elearning_platform project.
+WSGI config for educa project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elearning_platform.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
 
 application = get_wsgi_application()
