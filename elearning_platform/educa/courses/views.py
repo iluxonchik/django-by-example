72a142523f0a51f27dd738ebaab574464b20e8a9
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-18 16:48:07 +0100

    feat: add mixins and course managment views

--- elearning_platform/educa/courses/views.py
+++ elearning_platform/educa/courses/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Course
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-class ManageCourseListView(ListView):
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user  # this way we set the owner of the object automatically when it's saved
+        return super(OwnerEditMixin, self).form_valid(form)  # default behaviour is to save the model
+
+class OwnerCourseMixin(OwnerMixin):
     model = Course
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
 
-    def get_queryset(self):
-        qs = super(ManageCourseListView, self).get_queryset()
-        return qs.filter(owner=self.request.user)
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
